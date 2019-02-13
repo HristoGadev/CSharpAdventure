cec3376eba1581c8b535bc5dd6f3dcd0bf8ec9ab
HristoGadev <hrgadev32@gmail.com>
2019-02-13 23:27:41 +0200

    project files removed from server

--- Company/CompanyData/Configuration.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CompanyData
-{
-    public class Configuration
-    {
-        public const string ConfigurationString =
-            @"Server=DESKTOP-07DO2VN;Database=Company;Integrated Security=True";
-    }
-}
