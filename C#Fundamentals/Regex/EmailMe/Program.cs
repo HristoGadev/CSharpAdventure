4080c860d23c6149055d3bba8301bf98fbcc6317
HristoGadev <40789003+HristoGadev@users.noreply.github.com>
2019-02-13 22:36:06 +0200

    Add files via upload

---
+++ C#Fundamentals/Regex/EmailMe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"(.*)@(.*)";
+            var input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                var leftLenght = match.Groups[1].Value.Length;
+                var rightLenght = match.Groups[2].Value.Length;
+
+                if (leftLenght-rightLenght<0)
+                {
+                    Console.WriteLine("She is not the one.");
+                }
+                else
+                {
+                    Console.WriteLine("Call her!");
+                }
+            }
+
+
+        }
+    }
+}
